918bb8765cd57c58414010a6b7bedbce0b8fdf39
Homika Sirsate <sirsatehomika@gmail.com>
2026-08-26 17:30:53 +0530

    Add Google login route

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,23 +1,11 @@
-// import { Router } from "express";
-// import { addToHistory, getUserHistory, login, register } from "../controllers/user.controller.js";
-
-
-
-// const router = Router();
-
-// router.route("/login").post(login)
-// router.route("/register").post(register)
-// router.route("/add_to_activity").post(addToHistory)
-// router.route("/get_all_activity").get(getUserHistory)
-
-// export default router;
 import { Router } from "express";
 import {
     addToHistory,
     getUserHistory,
     login,
     register,
-    verifyEmail
+    verifyEmail,
+    googleLogin
 } from "../controllers/user.controller.js";
 
 const router = Router();
@@ -25,6 +13,7 @@ const router = Router();
 router.route("/login").post(login)
 router.route("/register").post(register)
 router.route("/verify-email").get(verifyEmail)
+router.route("/google").post(googleLogin)
 router.route("/add_to_activity").post(addToHistory)
 router.route("/get_all_activity").get(getUserHistory)
 

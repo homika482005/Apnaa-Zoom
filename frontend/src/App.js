eb01419fd3d246c380bfeca13714ac1712b835e3
Homika Sirsate <sirsatehomika@gmail.com>
2026-08-26 14:36:18 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Routes>
             <Route path='/' element={<LandingPage />} />
             <Route path='/auth' element={<Authentication />} />
-            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/home' element={<HomeComponent />} />
             <Route path='/history' element={<History />} />
             <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>

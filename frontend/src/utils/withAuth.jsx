95559191f0cbc819738a401b75e67621fe441ba4
Homika Sirsate <sirsatehomika@gmail.com>
2026-08-26 21:08:03 +0530

    Implement loading state in withAuth component
    Added loading state to AuthComponent for better user experience during authentication checks.

--- frontend/src/utils/withAuth.jsx
+++ frontend/src/utils/withAuth.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -10,6 +10,9 @@ const withAuth = (WrappedComponent) => {
     const AuthComponent = (props) => {
 
         const router = useNavigate();
+
+        const [loading, setLoading] =
+            useState(true);
 
 
         const checkAuthentication = async () => {
@@ -30,13 +33,15 @@ const withAuth = (WrappedComponent) => {
             try {
 
                 await axios.get(
-                    `${server}/api/v1/users/get_all_activity`,
+                    `${server}/api/v1/users/validate-session`,
                     {
                         params: {
                             token: token
                         }
                     }
                 );
+
+                setLoading(false);
 
             } catch (err) {
 
@@ -61,6 +66,24 @@ const withAuth = (WrappedComponent) => {
             checkAuthentication();
 
         }, []);
+
+
+        if (loading) {
+
+            return (
+                <div
+                    style={{
+                        height: "100vh",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}
+                >
+                    Loading...
+                </div>
+            )
+
+        }
 
 
         return (

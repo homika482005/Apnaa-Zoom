feb50d015d59e90610c81b006996c215f4c11795
Homika Sirsate <sirsatehomika@gmail.com>
2026-08-26 12:40:13 +0530

    Refactor user routes to include email verification

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,12 +1,30 @@
+// import { Router } from "express";
+// import { addToHistory, getUserHistory, login, register } from "../controllers/user.controller.js";
+
+
+
+// const router = Router();
+
+// router.route("/login").post(login)
+// router.route("/register").post(register)
+// router.route("/add_to_activity").post(addToHistory)
+// router.route("/get_all_activity").get(getUserHistory)
+
+// export default router;
 import { Router } from "express";
-import { addToHistory, getUserHistory, login, register } from "../controllers/user.controller.js";
-
-
+import {
+    addToHistory,
+    getUserHistory,
+    login,
+    register,
+    verifyEmail
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.route("/login").post(login)
 router.route("/register").post(register)
+router.route("/verify-email").get(verifyEmail)
 router.route("/add_to_activity").post(addToHistory)
 router.route("/get_all_activity").get(getUserHistory)
 

7a06da49d384154379b6526fcdc985b834ad7595
Homika Sirsate <sirsatehomika@gmail.com>
2026-08-26 20:39:25 +0530

    Add complete authentication routes

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,20 +1,51 @@
 import { Router } from "express";
+
 import {
     addToHistory,
     getUserHistory,
     login,
     register,
     verifyEmail,
-    googleLogin
+    resendVerificationEmail,
+    googleLogin,
+    forgotPassword,
+    resetPassword,
+    logout
 } from "../controllers/user.controller.js";
+
 
 const router = Router();
 
+
 router.route("/login").post(login)
+
 router.route("/register").post(register)
+
 router.route("/verify-email").get(verifyEmail)
+
+router.route("/resend-verification").post(
+    resendVerificationEmail
+)
+
 router.route("/google").post(googleLogin)
-router.route("/add_to_activity").post(addToHistory)
-router.route("/get_all_activity").get(getUserHistory)
+
+router.route("/forgot-password").post(
+    forgotPassword
+)
+
+router.route("/reset-password").post(
+    resetPassword
+)
+
+router.route("/logout").post(logout)
+
+router.route("/add_to_activity").post(
+    addToHistory
+)
+
+router.route("/get_all_activity").get(
+    getUserHistory
+)
+
 
 export default router;
